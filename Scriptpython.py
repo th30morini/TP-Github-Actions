3ce9f3dbfff5254eed00c45bbf710300307d574a
th30morini <theomorini@hotmail.com>
2025-06-23 15:51:58 +0200

    add soustraction

--- Scriptpython.py
+++ Scriptpython.py
@@ -6,6 +6,9 @@ class SimpleMath:
     def addition(a, b):
         return a + b
 
+    def soustraction(a, b):
+        return a - b
+
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         result = SimpleMath.addition(2, 3)
@@ -16,6 +19,17 @@ class TestSimpleMath(unittest.TestCase):
 
         result = SimpleMath.addition(0, 0)
         self.assertEqual(result, 0)
+
+    def test_soustraction(self):
+        result = SimpleMath.soustraction(2, 1)
+        self.assertEqual(result, 1)
+
+        result = SimpleMath.soustraction(8, 2)
+        self.assertEqual(result, 6)
+
+        result = SimpleMath.soustraction(3, 5)
+        self.assertEqual(result, -2)
+
 
 # Pour lancer le test
 if __name__ == '__main__':
